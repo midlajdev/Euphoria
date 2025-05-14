0759ce169d58f54b0d2d4776a2fd2395ec546804
Midlaj <midlajcv810@gmail.com>
2025-05-14 14:16:31 +0530

    functions added

--- js/script.js
+++ js/script.js
@@ -3,19 +3,17 @@ document.addEventListener('DOMContentLoaded', () => {
     const nav = document.querySelector('.main-nav');
     const bars = document.querySelectorAll('.bar');
     const himg = hamburger.querySelector('img')
-
     
-
     hamburger.addEventListener('click', (e) => {
         e.stopPropagation();
         // Toggle navigation
         nav.classList.toggle('active');
 
         if(nav.classList.contains('active')){
-            himg.src = './assets/icons/cancel-icon.png'
+            himg.src = '../assets/icons/cancel-icon.png'
         }
         else{
-            himg.src = './assets/icons/hamburger-icon.png'
+            himg.src = '../assets/icons/hamburger-icon.png'
         }
 
     });
@@ -24,9 +22,131 @@ document.addEventListener('DOMContentLoaded', () => {
     document.addEventListener('click', (e) => {
         if (!nav.contains(e.target) && nav.classList.contains('active')) {
             nav.classList.remove('active');
-            himg.src = './assets/icons/hamburger-icon.png'
+            himg.src = '../assets/icons/hamburger-icon.png'
 
         }
     });
+
+
+	//spotlight slide
+    const sliderLeft = document.querySelector('.prev-btn');
+    const sliderRight = document.querySelector('.next-btn');
+    const spotlight = document.getElementById('spotlight');
+    const images = ['../assets/images/bg-1.jpg','../assets/images/bg-2.jpg','../assets/images/bg-3.jpg']
+    let currentIndex = 0;
+    const progress = document.querySelector('.slider-progress');
+
+
+    function updateSlider() {
+      
+        // update progress bar
+        const progressLeft = (currentIndex / (images.length - 1)) * 67;
+        progress.style.left = `${progressLeft}%`;
+      }
+
+
+    sliderRight.addEventListener('click', (e) => {
+        currentIndex = (currentIndex + 1) % images.length; 
+        spotlight.style.backgroundImage = `url(${images[currentIndex]})`
+        updateSlider()
+
+    })
+
+    sliderLeft.addEventListener('click', (e) => {
+        currentIndex = (currentIndex - 1 + images.length) % images.length; 
+        spotlight.style.backgroundImage = `url(${images[currentIndex]})`
+        updateSlider()
+    })
+
+
+	//wishlist toggle function
+	const wishlists = document.querySelectorAll('.wishlist-btn img');
+
+    wishlists.forEach(wishlist =>{
+        wishlist.addEventListener('click', (e) => {
+            if(wishlist.src.includes('wishlist.svg')){
+                wishlist.src = '../assets/images/wishlist-fill.svg';
+            }
+            else{
+                wishlist.src = '../assets/images/wishlist.svg';
+            }
+        })
+    })
+
+
+	//footer categories-toggle function
+	const toggle = document.querySelector('.categories-toggle');
+	const expandableDiv = document.querySelector('.categories-expand');
+	const arrowIcon = document.querySelector('.toggle-arrow-icon');
+
+	toggle.addEventListener('click', (e) =>{
+		expandableDiv.classList.toggle('active');
+		arrowIcon.classList.toggle('rotate')
+		
+	})
+
+});
+
+
+
+//new arrivals slider
+const swiper = new Swiper('.swiper-container', {
+  // Optional parameters
+  loop: true,
+  slidesPerView:4,
+  centeredSlides: false,
+
+  breakpoints:{
+    0:{
+      slidesPerView:2,
+      spaceBetween:10,
+    },
+    640:{
+      slidesPerView:3,
+      spaceBetween:20,
+    },
+    1024:{
+      slidesPerView:4,
+      spaceBetween: 30
+    },
+  },
+
+  // Navigation arrows
+  navigation: {
+    nextEl: '.swiper-button-next',
+    prevEl: '.swiper-button-prev',
+  },
+
+});
+
+
+
+const swiper2 = new Swiper('.feedback-swiper-container', {
+  // Optional parameters
+  loop: true,
+  slidesPerView:2,
+  centeredSlides: false,
+  grabCursor: true,
+
+  breakpoints:{
+    0:{
+      slidesPerView:1,
+      spaceBetween:10,
+    },
+    640:{
+      slidesPerView:2,
+      spaceBetween:20,
+    },
+    1024:{
+      slidesPerView:3,
+      spaceBetween: 30
+    },
+  },
+
+  pagination: {
+    el: '.swiper-pagination',
+    clickable: true,
+    dynamicBullets:true,
+  },
 
 });
